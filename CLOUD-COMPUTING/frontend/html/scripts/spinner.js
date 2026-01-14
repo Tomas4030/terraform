5d617085cadd8dbb652adb1bb8fc6dca2412bec5
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2026-01-14 21:55:40 +0000

    Delete CLOUD-COMPUTING directory

--- CLOUD-COMPUTING/frontend/html/scripts/spinner.js
+++
@@ -1,7 +0,0 @@
-export function showSpinner() {
-  document.getElementById("loading-spinner").classList.remove("hidden");
-}
-
-export function hideSpinner() {
-  document.getElementById("loading-spinner").classList.add("hidden");
-}
